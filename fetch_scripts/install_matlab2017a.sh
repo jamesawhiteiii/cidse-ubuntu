a7f96bd6afea1645988c38f84309dab586852bf4
James A. White III <jwhite40@asu.edu>
2018-09-10 13:25:22 -0700

    UPDATED MATLAB SCRIPTS

---
+++ fetch_scripts/install_matlab2017a.sh
@@ -0,0 +1,56 @@
+#|**********************************************************************;
+# Project           : Install Matlab 2018a
+#
+# Program name      : fetch_matlab2018.sh
+#
+# Author            : James A. White III
+#
+# Date created      : Sep 10, 2018
+#
+# Purpose           : make it easy to make scripts that run on remote systems
+#                        1. Enter the commands where it tells you  and save it as a new file.
+#                        2. Done
+#
+#**********************************************************************;
+echo "This script will deploy MATLAB R2018a to the intended host system"
+
+#### Get Host Name ####
+echo 'enter hostname in "enXXXXXXXl" format'
+read HOST
+HOST='techs@'$HOST'.cidse.dhcp.asu.edu'
+
+#### Connect to the System ####
+ssh -t $HOST '
+echo 'Please enter Techs password When Prompted'
+
+##########################################################################
+# REMOTE COMMANDS
+##########################################################################
+
+
+##########################################################################
+# CHANGE TO /TMP
+##########################################################################
+
+cd /tmp/
+
+
+##########################################################################
+# DOWNLOAD LIVE INSTALLER FILE FORM GITHUB REPO
+##########################################################################
+
+wget https://raw.githubusercontent.com/jamesawhiteiii/cidse-ubuntu/master/software/install_matlab2017a.sh
+
+
+##########################################################################
+# SET PERMISSIONS
+##########################################################################
+
+chmod u+x /tmp/matlab2018a.sh
+
+##########################################################################
+# RUN LIVE INSTALLER
+##########################################################################
+
+bash /tmp/install_matlab2017a.sh --verbose
+'
