c0eaf22f298d7b7ddc420da1adb63a55f8a95cb0
jamesawhiteiii <26724870+jamesawhiteiii@users.noreply.github.com>
2019-01-30 12:18:17 -0700

    Uploaded version 15 of the VMWare script

---
+++ scripts/VMwear install 15.sh
@@ -0,0 +1,9 @@
+sudo apt-get update -y
+sudo apt-get install gcc build-essential linux-headers-$(uname -r) -y
+cd /tmp
+wget -O ~/vmware.bin https://www.vmware.com/go/getworkstation-linux
+sudo bash ~/vmware.bin
+
+## Key
+
+## HH6AH-LZ38L-580DD-0T186-0EV30
